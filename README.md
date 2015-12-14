50dd085e32516c0f06e0dd5931fb03b4c24b72da
Marius Mailat <marius.mailat@appsrise.com>
2015-12-14 15:50:36 +0300

    Initial version Yamba Application

--- README.md
+++
@@ -1,2 +0,0 @@
-# bootcamp-2015-12-14
-Android Bootcamp in Nizhny
---
+++ YambaNovgorod/app/src/main/java/com/intel/nizhny/yamba/StatusActivity.java
@@ -0,0 +1,20 @@
+package com.intel.nizhny.yamba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+
+    public void websiteOpenClick (View v)
+    {
+        //TODO start the cine link
+    }
+
+}
